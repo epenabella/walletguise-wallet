88629b1cee0c8ea62a41fd578ff8a50c0e07e2cf
Rick Penabella (2)  (Consultant) <rick.penabella.2@consultant.volvo.com>
2025-04-25 01:04:54 +0200

    init adapted to svelte

---
+++ svelte.config.mjs
@@ -0,0 +1,11 @@
+import preprocess from "svelte-preprocess"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+};
+
+export default config;
